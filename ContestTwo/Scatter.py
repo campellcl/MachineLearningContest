c666168875d374654b4f07c48c3879215d975649
Chris Campell <ccampell.asu@gmail.com>
2017-06-27 13:56:54 -0400

    Added scatter plot visualization for the second contest, looks promising for a decision tree.

---
+++ ContestTwo/Scatter.py
@@ -0,0 +1,28 @@
+"""
+Scatter.py
+Displays data prior to model construction.
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Read training data:
+    df_train = pd.read_csv('kaggle_02_train.csv', index_col='Id')
+    # Read testing data:
+    df_test = pd.read_csv('kaggle_02_test.csv', index_col='Id')
+    # Partition into train_x and train_y:
+    x = df_train[['x_1', 'x_2']].values
+    y = df_train['y'].values
+    # Plot the data for visualization:
+    index = y > 0
+    plt.scatter(x[index, 0], x[index, 1], label='positive')
+    plt.scatter(x[~index, 0], x[~index, 1], label='negative')
+    plt.legend()
+    plt.axis('equal')
+    plt.xlabel('x_1')
+    plt.ylabel('x_2')
+    plt.show()
+    # Partition into training and testing data:
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
